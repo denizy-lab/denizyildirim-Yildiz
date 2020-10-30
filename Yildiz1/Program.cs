1dc3a9bf67f6b01e91a52293932b308651ebdf91
IR0NNTi <IR0NNTi@DESKTOP-960OI32>
2020-10-30 23:47:54 +0300

    Importing project

---
+++ Yildiz1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Yildiz1
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            for (int i = 5; i >= 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
